2aa98002209ddbd5efc013c9ff010d4b37f38639
theos <theossoft@gmail.com>
2023-06-24 12:31:40 +0300

    Семинар 3. Задача 3. Нет решения.

---
+++ Lessons/src/main/java/Seminar3/Task003.java
@@ -0,0 +1,9 @@
+package Seminar3;
+//Создать список типа ArrayList<String>
+//Поместить в него как строки, так и целые числа
+//Пройти по списку, найти и удалить целые числа
+public class Task003 {
+    public static void main(String[] args) {
+
+    }
+}
